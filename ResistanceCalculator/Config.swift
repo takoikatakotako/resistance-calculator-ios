d563d1dd0d0b349cc14f1e8507a8c18925595a5b
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2021-02-12 17:19:50 +0900

    Config画面からページを開くようにした。

---
+++ ResistanceCalculator/Config.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+let TWITTER_URL: String = "https://twitter.com/takoikatakotako"
+let CONTACT_FORM_URL:String = "https://docs.google.com/forms/d/1UjPjJy1MdEe6vkNVF0V7gncF78vQFjDiCWMl8Q1PW7I"
+
--- ResistanceCalculator/View/Config/ConfigView.swift
+++ ResistanceCalculator/View/Config/ConfigView.swift
@@ -5,15 +5,21 @@ struct ConfigView: View {
         NavigationView {
             List {
                 ConfigRow(text: "作者のTwitterを開く") {
-                    
+                    openUrl(urlString: TWITTER_URL)
                 }
                 
                 ConfigRow(text: "お問い合わせ") {
-                    
+                    openUrl(urlString: CONTACT_FORM_URL)
                 }
             }
             .background(Color.orange)
             .navigationBarTitle("Config", displayMode: .inline)
+        }
+    }
+    
+    func openUrl(urlString: String) {
+        if let url = URL(string: urlString) {
+            UIApplication.shared.open(url)
         }
     }
 }
